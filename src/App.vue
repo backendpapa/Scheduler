cae0862d6f9c20804ddafa49e246673e810cdd8b
Akeju Paul <akejupaul10@gmail.com>
2021-10-15 06:22:45 +0100

    scheduler

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,65 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="white"
+      flat
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+      <div class="mt-6">
+       <v-text-field prepend-inner-icon="mdi-magnify" flat  placeholder="Search Categories" class="text-caption" clearable solo dense></v-text-field>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-btn color="#70D25F" style="text-transform:none" class="text-caption" dark>Create <v-icon>mdi-chevron-down</v-icon></v-btn>
+      <v-btn plain class="ml-4 mr-2" small icon><v-icon>mdi-bell-badge-outline</v-icon></v-btn>
+
     </v-app-bar>
+
+    <v-navigation-drawer
+        permanent
+        width="200"
+        color="#0286C2"
+        app
+      >
+       <div style="height:10.3vh;background-color:#0078AE;color:white" class="">
+         <v-row no-gutters style="height:100%" align="center" justify="center">
+           <v-avatar size="40">
+             <v-img src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80"></v-img>
+           </v-avatar>
+           <div class="ml-1 mt-4">
+             <p class="text-caption mb-n2 font-weight-bold">Backend Papa</p>
+             <p class="text-caption ">Abuja</p>
+           </div>
+           <div>
+             <v-btn icon small plain><v-icon color="white">mdi-chevron-down</v-icon></v-btn>
+           </div>
+         </v-row>
+       </div>
+
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
     <v-main>
       <router-view/>
@@ -44,7 +69,7 @@
 </template>
 
 <script>
-
+import "./App.css"
 export default {
   name: 'App',
 
